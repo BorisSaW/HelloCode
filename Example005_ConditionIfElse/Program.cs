3efae50b6b2c80e148d305b6c9933653525fa7d9
Boris <burbon1821@yandex.ru>
2022-08-24 07:28:43 +0300

    add Example 005  Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "борис")
+{
+    Console.WriteLine("Ура, это же Борис!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
